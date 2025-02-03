5c83595d54f90243c5a0f220c7963acfe1c5ffa5
Gajanika <premalathaganeshalingam@gmail.com>
2025-02-03 14:34:11 +0530

    OOP

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+    
+
+public class  Main{
+
+    int x=5;
+    public String fname;
+    public String age;
+    public String Iname;
+
+public static void main (String[] args){
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+}
+}
---
+++ Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+    String fname = "gaju";
+    String Iname= "Jeni";
+    int age =21;
+
+  public static void main (String [] args){
+    
+       
+    Practice myObj = new Practice();
+    System.out.println("Name:" + myObj.fname + " "+ myObj.Iname);
+    System.out.println("Age:" + myObj.age);
+
+  }
+
+
+}
