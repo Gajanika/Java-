10fbb2f88d7ed6ba5c4920e7eafce43d0b300228
Gajanika <premalathaganeshalingam@gmail.com>
2025-02-05 11:41:46 +0530

    calling Methods with creating objects

---
+++ Area.java
@@ -0,0 +1,13 @@
+public class Area {
+    
+    public static void main(String[] args) {
+        int x=5;
+        int y=4;
+        Main area = new Main();
+        System.out.println(x*y);
+
+    }
+
+
+}
+public class parapu{}
---
+++ parapu.java
@@ -0,0 +1,13 @@
+public class parapu {
+    
+    public static void main(String[] args) {
+        int x=5;
+        int y=4;
+        Main area = new Main();
+        System.out.print("Area of Reatangle:");
+        Main areaa = new Main();
+        System.out.println(x*y);
+
+    }
+    
+}
