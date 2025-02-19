cc0606ce36b2be1cf5481480b3afa220c58c760d
Gajanika <premalathaganeshalingam@gmail.com>
2025-02-19 08:00:14 +0530

    Added Const class with variables and a method to print default values.

--- 2helloo.java
+++
@@ -1,7 +0,0 @@
-  class helloo{
-    public static void main(String args[]){
-        System.out.print("hello world,");
-        System.out.print("I am, ");
-        System.out.print("hello world");
-    }
-}
