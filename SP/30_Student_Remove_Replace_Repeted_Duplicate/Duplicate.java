c05aefb788752ce9eb4f8b65f951a3309a2779bc
Gajanika <premalathaganeshalingam@gmail.com>
2025-02-21 14:03:44 +0530

    Duplicate

---
+++ SP/30_Student_Remove_Replace_Repeted_Duplicate/Duplicate.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class dupli {
+    public static void main(String[] args) {
+        ArrayList<String> original = new ArrayList<>();
+        original.add("ssda");
+        original.add("lll");
+        original.add("dfff");
+        original.add("S");
+        original.add("AA");
+        ArrayList<String> duooo = new ArrayList<>(original);
+        System.out.println("Original:" + original);
+        System.out.println("Duplicate:" + duooo);
+
+    }  
+
+}
