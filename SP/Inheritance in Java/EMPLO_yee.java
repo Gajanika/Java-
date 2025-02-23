83fa23ccae7f9ff1bc201b3bef6d578e3ca4187e
Gajanika <premalathaganeshalingam@gmail.com>
2025-02-23 09:14:48 +0530

    Java Object Creation of Inherited Class

---
+++ SP/Inheritance in Java/EMPLO_yee.java
@@ -0,0 +1,23 @@
+
+
+import java.io.*;
+
+
+class Employee {
+    int salary = 60000;
+}
+
+
+class Engineer extends Employee {
+    int benefits = 10000;
+}
+
+class EMPLO_yee {
+    public static void main(String args[])
+    {
+        Engineer E1 = new Engineer();
+        System.out.println("Salary : " + E1.salary
+                           + "\nBenefits : " + E1.benefits);
+    }
+}
+
--- SP/Inheritance in Java/Test.java
+++ SP/Inheritance in Java/Test.java
@@ -1,20 +1,19 @@
-// Java program to illustrate the
-// concept of inheritance
 
-// base class
+
+
 class Bicycle {
-    // the Bicycle class has two fields
+    
     public int gear;
     public int speed;
 
-    // the Bicycle class has one constructor
+    
     public Bicycle(int gear, int speed)
     {
         this.gear = gear;
         this.speed = speed;
     }
 
-    // the Bicycle class has three methods
+    
     public void applyBrake(int decrement)
     {
         speed -= decrement;
@@ -25,7 +24,7 @@ class Bicycle {
         speed += increment;
     }
 
-    // toString() method to print info of Bicycle
+  
     public String toString()
     {
         return ("No of gears are " + gear + "\n"
@@ -33,29 +32,28 @@ class Bicycle {
     }
 }
 
-// derived class
+
 class MountainBike extends Bicycle {
 
-    // the MountainBike subclass adds one more field
+   
     public int seatHeight;
 
-    // the MountainBike subclass has one constructor
+    
     public MountainBike(int gear, int speed,
                         int startHeight)
     {
-        // invoking base-class(Bicycle) constructor
+        
         super(gear, speed);
         seatHeight = startHeight;
     }
 
-    // the MountainBike subclass adds one more method
+    
     public void setHeight(int newValue)
     {
         seatHeight = newValue;
     }
 
-    // overriding toString() method
-    // of Bicycle to print more info
+    
     @Override public String toString()
     {
         return (super.toString() + "\nseat height is "
@@ -63,7 +61,7 @@ class MountainBike extends Bicycle {
     }
 }
 
-// driver class
+
 public class Test {
     public static void main(String args[])
     {
