89c86f20b715c8ebd5353c7b1c4654b9578f6810
Gajanika <premalathaganeshalingam@gmail.com>
2025-02-25 09:43:07 +0530

    Introducing Control Statements about the if and the for loop.

---
+++ SP/Bank Functionality/CentralBank.java
@@ -0,0 +1,11 @@
+public interface CentralBank {
+    long getBalance();
+
+    void withdraw(long amount);
+
+    void deposit(long amount);
+
+    void showAccount();
+
+    boolean search(String acc_no);
+}
